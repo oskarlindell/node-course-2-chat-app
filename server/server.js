d0e678a72f265537807d32ba731f3227262a0b50
Oskar Lindell <oskar@oskarlindell.com>
2020-04-05 21:18:27 +0200

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -24,6 +24,18 @@ io.on("connection", socket => {
   //   createdAt: 123123
   // });
 
+  socket.emit("newMessage", {
+    from: "Admin",
+    text: "Welcome to the chat app",
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit("newMessage", {
+    from: "Admin",
+    text: "New user joined",
+    createdAt: new Date().getTime()
+  });
+
   socket.on("createMessage", message => {
     console.log("createMessage", message);
     io.emit("newMessage", {
@@ -31,6 +43,11 @@ io.on("connection", socket => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit("newMessage", {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on("disconnect", function() {
